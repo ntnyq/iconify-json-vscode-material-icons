3566970b8ed67950e62e55bcfdf27807ab7e8d0f
ntnyq <ntnyq13@gmail.com>
2025-04-19 10:54:42 +0800

    chore: update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import { defineESLintConfig } from '@ntnyq/eslint-config'
 
 export default defineESLintConfig({
   ignores: ['**/vscode-material-icon-theme/**'],
+  pnpm: true,
   unocss: true,
   vue: true,
   perfectionist: {
